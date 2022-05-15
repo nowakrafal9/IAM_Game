07253b18b7f13190408e55ae5bb83357d909dc81
nowakrafal9 <nowakr99@gmail.com>
2022-05-15 11:31:55 +0200

    Modified Overworld and GameObject class

--- scripts/GameObject.js
+++ scripts/GameObject.js
@@ -2,10 +2,17 @@ class GameObject {
     constructor(config) {
         this.x = config.x || 0;
         this.y = config.y || 0;
+        this.sizeX = config.sizeX;
+        this.sizeY = config.sizeY;
         this.direction = config.direction || "right"
+        this.objectType = config.objectType || null;
+
         this.sprite = new Sprite({
             gameObject: this,
             src: config.src,
+
+            sizeX: this.sizeX,
+            sizeY: this.sizeY,
         });
     }
     update() { }
