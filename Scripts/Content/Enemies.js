7aaa6576ab2170fd59a638c5e72e0c8c5ffd1e8d
nowakrafal9 <nowakr99@gmail.com>
2022-05-24 12:12:45 +0200

    Modified icons

--- Scripts/Content/Enemies.js
+++ Scripts/Content/Enemies.js
@@ -6,31 +6,31 @@ window.Enemies = {
     "slime_normal": {
         name: "Slime",
         type: EnemyTypes.slime,
-        icon: "./Assets/characters/icons/Slime_icon.png",
+        icon: "./Assets/characters/icons/Enemy_icon.png",
         actions: ["FastAttack"]
     },
     "slime_fire": {
         name: "Fire Slime",
         type: EnemyTypes.slime,
-        icon: "./Assets/characters/icons/Slime_icon.png",
+        icon: "./Assets/characters/icons/Enemy_icon.png",
         actions: ["FastAttack", "SlimeFireAttack"]
     },
     "slime_poison": {
         name: "Poison Slime",
         type: EnemyTypes.slime,
-        icon: "./Assets/characters/icons/Slime_icon.png",
+        icon: "./Assets/characters/icons/Enemy_icon.png",
         actions: ["FastAttack", "SlimePoisonAttack"]
     },
     "slime_ice": {
         name: "Ice Slime",
         type: EnemyTypes.slime,
-        icon: "./Assets/characters/icons/Slime_icon.png",
+        icon: "./Assets/characters/icons/Enemy_icon.png",
         actions: ["FastAttack"]
     },
     "slime_boss": {
         name: "Slime Boss",
         type: EnemyTypes.slime,
-        icon: "./Assets/characters/icons/SlimeBoss_icon.png",
+        icon: "./Assets/characters/icons/Enemy_icon.png",
         actions: ["FastAttack", "SlimePoisonAttack", "SlimeFireAttack"]
     }
 }
